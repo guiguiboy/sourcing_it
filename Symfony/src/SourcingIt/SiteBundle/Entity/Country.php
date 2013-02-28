2ea8edb101ff249b69a92c69f83940f58cfb8657
user <user@squeeze.travelcom.michelin-travel.com>
2013-02-28 18:32:31 +0100

    Added new Entities Company + Country

---
+++ Symfony/src/SourcingIt/SiteBundle/Entity/Country.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace SourcingIt\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Country
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="SourcingIt\SiteBundle\Entity\CountryRepository")
+ */
+class Country
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=3)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="currency_code", type="string", length=3)
+     */
+    private $currency_code;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Country
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Country
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set currency_code
+     *
+     * @param string $currencyCode
+     * @return Country
+     */
+    public function setCurrencyCode($currencyCode)
+    {
+        $this->currency_code = $currencyCode;
+    
+        return $this;
+    }
+
+    /**
+     * Get currency_code
+     *
+     * @return string 
+     */
+    public function getCurrencyCode()
+    {
+        return $this->currency_code;
+    }
+}
